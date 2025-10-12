6b023976e01720d5ffe0e1ca9337723b19360f09
kimhy <kimhyyefla@gmail.com>
2025-10-12 20:38:23 +0900

    Fix: Correct typo in `_cameras` variable declaration.

--- lib/week04/video_call/camera_example.dart
+++ lib/week04/video_call/camera_example.dart
@@ -1,7 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-late List<CameraDescription> _ cameras;
+late List<CameraDescription> _cameras;
 
 Future<void> main() async {
   // Flutter 앱이 실행될 준비가 됐는지 확인
@@ -10,6 +10,5 @@ Future<void> main() async {
   // 핸드폰에 있는 카메라들 가져오기
   _cameras = await availableCameras();
   runApp(const CameraApp());
-
-
 }
+
