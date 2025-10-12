d04f27262170306a27dd003a7e3c59b8bdb95321
kimhy <kimhyyefla@gmail.com>
2025-10-13 02:42:23 +0900

    Updated Changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = flutter.targetSdkVersion
+        multiDexEnabled true
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
