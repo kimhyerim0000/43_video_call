5e7ed9d45ddab79f20b3c47be0888fb289c28179
kimhy <kimhyyefla@gmail.com>
2025-10-12 21:20:46 +0900

    feat: Implement basic CamScreen UI

--- lib/week04/video_call/screen/cam_screen.dart
+++ lib/week04/video_call/screen/cam_screen.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
 
-clas
+class CamScreen extends StatefulWidget {
+  const CamScreen({Key? key}) : super(key: key);
+
+  @override
+  _CamScreenState createState() => _CamScreenState();
+}
+
+class _CamScreenState extends State<CamScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LIVE'),
+      ),
+      body: Center(
+        child: Text('Cam Screen'),
+      ),
+    );
+  }
+}
