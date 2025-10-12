79a27153e138d084ce6b6e4440aea936f9be4cc8
kimhy <kimhyyefla@gmail.com>
2025-10-12 20:32:24 +0900

    feat: Implement basic calendar scheduler UI

---
+++ lib/week05/calendar_scheduler/component/today_banner.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/week05/calendar_scheduler/const/colors.dart'; 
+
+class TodayBanner extends StatelessWidget {
+  final DateTime selectedDate;
+  final int count;
+
+  const TodayBanner({
+    required this.selectedDate,
+    required this.count,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16.0,
+      color: Colors.white,
+    );
+
+    return Container(
+      color: PRIMARY_COLOR,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일',
+              style: textStyle,
+            ),
+            Text(
+              '$count개',
+              style: textStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
