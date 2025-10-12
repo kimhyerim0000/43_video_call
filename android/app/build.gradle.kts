b56b6f69725a4963113a11b0fef4875fc51c5c7f
kimhy <kimhyyefla@gmail.com>
2025-10-13 06:05:07 +0900

    Updated Changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,12 +24,14 @@ android {
         applicationId = "com.example.myapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        targetSdk = 21
+        minSdk = 21
         //minSdk = flutter.minSdkVersion
         //targetSdk = flutter.targetSdkVersion
-        multiDexEnabled =true
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        multiDexEnabled = true
     }
 
     buildTypes {
