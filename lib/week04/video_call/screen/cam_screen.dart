573e34c0a814cf09c2d6eaf6f0d965f1365d5347
kimhy <kimhyyefla@gmail.com>
2025-10-12 21:28:29 +0900

    feat(cam_screen): Add camera and microphone permission handling

--- lib/week04/video_call/screen/cam_screen.dart
+++ lib/week04/video_call/screen/cam_screen.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class CamScreen extends StatefulWidget {
-  Future<bool> init() async {
+  Future<bool> init() async { // 권한 관련 작업 모두 실행
     final resp = await [Permission.camera, Permission.microphone].requres();
 
     final cameraPermission = resp[Permission.camera];
     final micPermission = resp[Permission.microphone];
 
     if(cameraPermission != PermissionStatus.granted ||
-        micPermission !=)
+        micPermission != PermissionStatus.granted) {
+          throw '카메라 또는 마이크 권한이 없습니다.';
+        }
+        return true;
   }
   const CamScreen({Key? key}) : super(key: key);
 
