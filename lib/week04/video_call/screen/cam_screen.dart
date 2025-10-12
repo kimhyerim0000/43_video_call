35737524ed58d9f51dce3c236f47a24a0c5d1748
kimhy <kimhyyefla@gmail.com>
2025-10-12 21:47:27 +0900

    Updated Changes

--- lib/week04/video_call/screen/cam_screen.dart
+++ lib/week04/video_call/screen/cam_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:agora_rtc_engine/agora_ric_engine.dart';
-import 'package:video_call/const/agora.dart';
+import 'package:myapp/week04/video_call/const/agora.dart';
 
 class CamScreen extends StatefulWidget {
   
