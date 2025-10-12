b86eda583db1d0638de1359e0adb75b0c636e21a
kimhy <kimhyyefla@gmail.com>
2025-10-13 06:13:30 +0900

    Fix: Use flutter.minSdkVersion for minSdk in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.myapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         //minSdk = flutter.minSdkVersion
         //targetSdk = flutter.targetSdkVersion
         targetSdk = 33
