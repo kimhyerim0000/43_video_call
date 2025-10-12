a88011cd2c6cd5080effb0b0d9b8ff0c83919946
kimhy <kimhyyefla@gmail.com>
2025-10-12 20:37:53 +0900

    feat(camera_example): Initialize and retrieve available cameras

--- lib/week04/video_call/camera_example.dart
+++ lib/week04/video_call/camera_example.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 late List<CameraDescription> _ cameras;
 
 Future<void> main() async {
+  // Flutter 앱이 실행될 준비가 됐는지 확인
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 핸드폰에 있는 카메라들 가져오기
+  _cameras = await availableCameras();
   runApp(const CameraApp());
+
+
 }
